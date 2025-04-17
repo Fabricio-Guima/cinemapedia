53c7475c1187a9569b5c8c944bb5cccda5b76243
Fabricio-Guima <fsgkof@gmail.com>
2025-04-17 20:21:38 -0300

    create  movieDetailsToEntity mapper

--- lib/infrastructure/mappers/movie_mapper.dart
+++ lib/infrastructure/mappers/movie_mapper.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/domain/entities/movie.dart';
+import 'package:cinemapedia/infrastructure/models/moviedb/movie_details.dart';
 import 'package:cinemapedia/infrastructure/models/moviedb/movie_moviedb.dart';
 
 class MovieMapper {
@@ -22,4 +23,25 @@ class MovieMapper {
       voteAverage: moviedb.voteAverage,
       voteCount: moviedb.voteCount
       );
+
+  static Movie movieDetailsToEntity( MovieDetails moviedb ) => Movie(
+     adult: moviedb.adult,
+      backdropPath: (moviedb.backdropPath != '')
+        ? 'https://image.tmdb.org/t/p/w500/${ moviedb.backdropPath }' 
+        : 'https://img.freepik.com/premium-vector/error-404-page-found-banners-website_837419-291.jpg',
+      genreIds: moviedb.genres.map((e) => e.name).toList(),
+      id: moviedb.id,
+      originalLanguage: moviedb.originalLanguage,
+      originalTitle: moviedb.originalTitle,
+      overview: moviedb.overview,
+      popularity: moviedb.popularity,
+      posterPath: (moviedb.posterPath != '')
+        ? 'https://image.tmdb.org/t/p/w500/${ moviedb.backdropPath }'
+        : 'no-poster',
+      releaseDate: moviedb.releaseDate,
+      title: moviedb.title,
+      video: moviedb.video,
+      voteAverage: moviedb.voteAverage,
+      voteCount: moviedb.voteCount
+  );
 }
