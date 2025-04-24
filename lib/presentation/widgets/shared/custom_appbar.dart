263142c6ea5c97d51e735f35e8860f7aabd35254
Fabricio-Guima <fsgkof@gmail.com>
2025-04-24 14:51:32 -0300

    SearchMovieDelegate

--- lib/presentation/widgets/shared/custom_appbar.dart
+++ lib/presentation/widgets/shared/custom_appbar.dart
@@ -1,3 +1,4 @@
+import 'package:cinemapedia/presentation/delegates/search_movie_delegate.dart';
 import 'package:flutter/material.dart';
 
 class CustomAppbar extends StatelessWidget {
@@ -24,7 +25,12 @@ class CustomAppbar extends StatelessWidget {
                 const Spacer(),
         
                 IconButton(
-                  onPressed: () {}, 
+                  onPressed: () {
+                    showSearch(
+                      context: context, 
+                      delegate: SearchMovieDelegate()
+                    );
+                  }, 
                   icon: const Icon(Icons.search)
                 )
               ],
