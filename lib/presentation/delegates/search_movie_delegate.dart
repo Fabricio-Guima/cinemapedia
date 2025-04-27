a599bb0197a607a3a5b3545d2cbc5b4c30860b9a
Fabricio-Guima <fsgkof@gmail.com>
2025-04-27 16:42:44 -0300

    show image, description and rating at Movie list

--- lib/presentation/delegates/search_movie_delegate.dart
+++ lib/presentation/delegates/search_movie_delegate.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:animate_do/animate_do.dart';
+import 'package:cinemapedia/config/helpers/human_formats.dart';
 import 'package:flutter/material.dart';
 
 import 'package:cinemapedia/domain/entities/movie.dart';
@@ -54,13 +55,90 @@ class SearchMovieDelegate extends SearchDelegate<Movie?> {
         return ListView.builder(   
           itemCount: movies.length,       
           itemBuilder: (context, index) {
-            final movie = movies[index];
-            return ListTile(
-              title: Text(movie.title),
-            );
+            return _MovieItem(movie: movies[index]);           
           },
         );
       },
     );
   }
 }
+
+class _MovieItem extends StatelessWidget {
+  final Movie movie;
+
+  const _MovieItem({required this.movie});
+
+  @override
+  Widget build(BuildContext context) {
+    final textStyles = Theme.of(context).textTheme;
+    final size = MediaQuery.of(context).size;
+    print('movie.posterPath ${movie.posterPath}');
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+      child: Row(
+        children: [
+          SizedBox(
+            width: size.width * 0.2,
+            height: size.width * 0.2 * 1.5,
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: Image.network(
+                movie.posterPath.isNotEmpty
+                    ? movie.posterPath
+                    : 'https://fireteller.com.au/wp-content/uploads/2020/09/Poster_Not_Available2.jpg',
+                fit: BoxFit.cover,
+                loadingBuilder: (context, child, loadingProgress) {
+                  if (loadingProgress == null) return child;
+                  return const Center(
+                    child: CircularProgressIndicator(strokeWidth: 2),
+                  );
+                },
+                errorBuilder: (context, error, stackTrace) => Image.network(
+                  'https://fireteller.com.au/wp-content/uploads/2020/09/Poster_Not_Available2.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(width: 10),
+          SizedBox(
+            width: size.width * 0.7,
+           child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  movie.title,
+                  style: textStyles.titleMedium,
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  movie.overview.isNotEmpty
+                      ? (movie.overview.length > 100
+                          ? '${movie.overview.substring(0, 100)}...'
+                          : movie.overview)
+                      : '',
+                  style: textStyles.bodySmall,
+                ),
+                const SizedBox(height: 5),
+                Row(
+                  children: [
+                    Icon(Icons.star_half_rounded,
+                        color: Colors.yellow.shade800),
+                    const SizedBox(width: 5),
+                    Text(
+                      HumanFormats.number(movie.voteAverage, 1),
+                      style: textStyles.bodyMedium!.copyWith(color: Colors.yellow.shade900),
+                      ),
+                  ],
+                ),
+              ],
+            ),
+
+          ),
+        ],
+      ),
+    );
+  }
+}
+
